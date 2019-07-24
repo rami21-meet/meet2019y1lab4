3c9a674a96647e314410a50a6b296ba97c487be5
rami21-meet <rami21@meet.mit.edu>
2019-07-24 05:27:53 -0400

    Stopping here

---
+++ lab_4.py
@@ -0,0 +1,41 @@
+"""
+import turtle
+zbale=turtle.clone()
+turtle.bgcolor('red')
+turtle.color('green')
+turtle.pensize(15)
+turtle.shape('triangle')
+zbale.color('black')
+zbale.pensize(15)
+zbale.shape('turtle')
+turtle.pendown
+turtle.goto(0,-150)
+turtle.goto(75,-150)
+turtle.color('blue')
+turtle.goto(75,0)
+turtle.left(90)
+turtle.stamp()
+zbale.goto(0,150)
+zbale.goto(-75,150)
+zbale.color('white')
+zbale.goto(-75,0)
+zbale.stamp()
+turtle.mainloop()
+"""
+import turtle
+turtle.pendown
+def up():
+    turtle.left(90)
+    turtle.forward(100)
+    
+def down:
+    turtle.right(90)
+    turtle.forward(100)
+    
+def left:
+    turtle.left(180)
+    turtle.forward(100)
+
+ def right:
+     turtle.forward(100)
+     
